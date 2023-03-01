af8598b5986d3f483a4cc6d7bbb19e99e8025a35
Brandon A <brandonwalex@gmail.com>
2023-03-01 12:54:34 -0600

    Update FollowerTracker.py

--- FollowerTracker.py
+++ FollowerTracker.py
@@ -1,72 +1,103 @@
-# THIS IS MADE FOR MACOS ONLY
-# This program is used to see names of accounts who have followed and who have unfollowed Instagram accounts recently.
-# This will also work for private accounts under 2 conditions.
-# 1. You log in with the private account you want to check
-# 2. The account you use to log in is following the private account you want to check.
-# If the account is public that you want to check, you can login with any account and
-# still check said accounts followers.
-# The code works by storing a set of all the followers that an account has in the Documents folder
-# of a MacOS computer
-# The next time the code is run, it will compare current followers to the followers we saved originally.
-# If the new followers is missing someone from the old followers, that means they unfollowed and that is 
-# output to the user. Vice versa is true for followers.
-
-import instaloader # Cool module very useful for instagram automation
+import instaloader
 import os
 
-# Login to instagram
-L = instaloader.Instaloader()
-USER=input('\nThis program needs an Instagram account to login to.\nIt can get followers of private accounts that the inputted account follows and followers of all public accounts.\n\nEnter your username: ')
-L.interactive_login(USER)
-print(f'\nLogged in as {USER}\n')
+L=instaloader.Instaloader()
 
-# Create directory for save data
-COMPUTER=os.path.expanduser('~')[7:]
-if not os.path.exists(f'/Users/{COMPUTER}/Documents/FollowerChecker'):
-    os.makedirs(f'/Users/{COMPUTER}/Documents/FollowerChecker')
+def login():
+    global USER
+    USER=input('\nThis program needs an Instagram account to login to.\nIt can get followers of private accounts that the inputted account follows and followers of all public accounts.\n\nEnter your username: ')
+    L.interactive_login(USER)
+    print(f'\nLogged in as {USER}\n')
 
-# Obtain profile metadata
-CHECKING=input(f'Whose followers do you want to check? (If checking own followers, just hit ENTER): ')
-USERNAME=CHECKING
-if CHECKING.strip()=='':
-    CHECKING=USER
-    USERNAME='you'
-print(f'\nLoading...\n')
-profile=instaloader.Profile.from_username(L.context,CHECKING)
+def obtainUser():
+    global USERNAME
+    global CHECKING
+    global PROFILE
+    USERNAME=input(f'Whose followers do you want to check? (If checking own followers, just hit ENTER): ')
+    if USERNAME.strip()=='':
+        CHECKING='you'
+        USERNAME=USER
+    else:
+        CHECKING=USERNAME
+    print(f'\nLoading...\n')
+    PROFILE=instaloader.Profile.from_username(L.context,USERNAME)
 
-# Print list of followers
-follower_set=set()
-for follower in profile.get_followers():
-    follower_set.add(follower.username)
-if not os.path.exists(f'/Users/{COMPUTER}/Documents/FollowerChecker/{CHECKING}\'sfollowers.txt'):
-    file=open(f'/Users/{COMPUTER}/Documents/FollowerChecker/{CHECKING}\'sfollowers.txt','w')
+def checkDir():
+    global COMPUTER
+    COMPUTER=os.path.expanduser('~')[7:]
+    if not os.path.exists(f'/Users/{COMPUTER}/Documents/FollowerChecker'):
+        os.makedirs(f'/Users/{COMPUTER}/Documents/FollowerChecker')
+
+def obtainFollowers():
+    global follower_set
+    follower_set=set()
+    for follower in PROFILE.get_followers():
+        follower_set.add(follower.username)
+
+def readFile():
+    global old_followers
+    file=open(f'/Users/{COMPUTER}/Documents/FollowerChecker/{USERNAME}\'sfollowers.txt','r')
+    old_followers=set(file.read().splitlines())
+    file.close()
+    file=open(f'/Users/{COMPUTER}/Documents/FollowerChecker/{USERNAME}\'sfollowers.txt','w')
+    for follower in follower_set:
+        file.write(f'{follower}\n')
+    file.close()
+
+def firstTime():
+    file=open(f'/Users/{COMPUTER}/Documents/FollowerChecker/{USERNAME}\'sfollowers.txt','w')
     for follower in follower_set:
         file.write(f'{follower}\n')
     file.close()
     print(f'{len(follower_set)} people are following {CHECKING}')
-    print(f'Followers list saved to {CHECKING}\'sfollowers.txt in Documents')
-    print(f'\nStarted tracking {CHECKING}\'s followers.')
-else:
-    file=open(f'/Users/{COMPUTER}/Documents/FollowerChecker/{CHECKING}\'sfollowers.txt','r')
-    old_followers=set(file.read().splitlines())
-    file.close()
-    file=open(f'/Users/{COMPUTER}/Documents/FollowerChecker/{CHECKING}\'sfollowers.txt','w')
-    for follower in follower_set:
-        file.write(f'{follower}\n')
-    file.close()
+    print(f'Followers list saved to {USERNAME}\'sfollowers.txt in Documents')
+    print(f'\nStarted tracking {USERNAME}\'s followers.')
+
+def printFollowers():
     new_followers=follower_set-old_followers
     for people in new_followers:
-        print(f'{people} has followed {USERNAME}')
+        print(f'{people} has followed {CHECKING}')
     if len(new_followers)==1:
-        print(f'{1} person has followed {USERNAME}\n')
+        print(f'{1} person has followed {CHECKING}\n')
     else:
-        print(f'{len(new_followers)} people have followed {USERNAME}\n')
+        print(f'{len(new_followers)} people have followed {CHECKING}\n')
+
+def printUnfollowers():
     only_old_followers=old_followers-follower_set
     for people in only_old_followers:
-        print(f'{people} has unfollowed {USERNAME}')
+        print(f'{people} has unfollowed {CHECKING}')
     if len(only_old_followers)==1:
-        print(f'{1} person has unfollowed {USERNAME}')
+        print(f'{1} person has unfollowed {CHECKING}')
     else:
-        print(f'{len(only_old_followers)} people have unfollowed {USERNAME}')
-    print(f'\n{len(follower_set)} people are following {USERNAME}')
-    print(f'Followers list saved to {CHECKING}\'s_followers.txt in Documents\n')
+        print(f'{len(only_old_followers)} people have unfollowed {CHECKING}')
+
+def printNonReciprocators():
+    print()
+    following_set=set()
+    for follower in PROFILE.get_followees():
+        following_set.add(follower.username)
+    not_following_back=following_set-follower_set
+    for people in not_following_back:
+        print(f'{people} is not following {CHECKING} back')
+    print(f'\n{len(not_following_back)} people are not following {CHECKING} back')
+
+def main():
+    login()
+    checkDir()
+    obtainUser()
+    obtainFollowers()
+    if not os.path.exists(f'/Users/{COMPUTER}/Documents/FollowerChecker/{USERNAME}\'sfollowers.txt'):
+        firstTime()
+    else:
+        readFile()
+        printFollowers()
+        printUnfollowers()
+        print(f'\n{len(follower_set)} people are following {CHECKING}')
+        print(f'Followers list saved to {USERNAME}\'s_followers.txt in Documents\n')
+    if CHECKING=='you':
+        reciprocators=input(f'Would you like to check who is not following you back? (y/n): ')
+        if reciprocators.strip().lower()=='y':
+            printNonReciprocators()
+
+if __name__=='__main__':
+    main()
